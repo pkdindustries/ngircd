e9071c9b8caa5780b77b10bb899ddbb5de77396a
alex <alex.schlessinger@gmail.com>
2023-01-07 02:54:02 -0800

    how did this ever push

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-docker build . -t gcr.io/ngircd/ngircd:dev 
-docker push gcr.io/ngircd/ngircd:dev
+docker build . -t gcr.io/linksnaps/ngircd:dev 
+docker push gcr.io/linksnaps/ngircd:dev
