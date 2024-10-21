61f96e65f6b288769407dfbc666c4dfc5c1aa3f1
alex schlessinger <alex.schlessinger@gmail.com>
2024-10-20 17:49:18 -0700

    cleanup

--- ngircd.sh
+++ ngircd.sh
@@ -1,59 +1,50 @@
 #!/bin/sh
 
 # Set default values
-if [ -z "$IRCD_NAME" ]; then
-    export IRCD_NAME="soulshack.irc"
-fi
+: "${IRCD_NAME:="soulshack.irc"}"
+: "${IRCD_INFO:="Default Info"}"
+: "${IRCD_MOTD:="Welcome to the IRC server!"}"
+: "${IRCD_NETWORK:="DefaultNetwork"}"
+: "${IRCD_PORTS:="6667"}"
 
-# Handle SSL configuration
-IRCD_SSL_CONFIG=""
-if [ -n "$IRCD_SSL_CERT_FILE" ]; then
-    IRCD_SSL_CONFIG="${IRCD_SSL_CONFIG}CertFile = ${IRCD_SSL_CERT_FILE}
-"
-fi
-if [ -n "$IRCD_SSL_KEY_FILE" ]; then
-    IRCD_SSL_CONFIG="${IRCD_SSL_CONFIG}KeyFile = ${IRCD_SSL_KEY_FILE}
-"
-fi
-if [ -n "$IRCD_SSL_KEYFILE_PASSWORD" ]; then
-    IRCD_SSL_CONFIG="${IRCD_SSL_CONFIG}KeyFilePassword = ${IRCD_SSL_KEYFILE_PASSWORD}
-"
-fi
-if [ -n "$IRCD_SSL_PORTS" ]; then
-    IRCD_SSL_CONFIG="${IRCD_SSL_CONFIG}Ports = ${IRCD_SSL_PORTS}
-"
-fi
+# Write the configuration file
+cat <<EOF > /ngircd.conf
+[Global]
+Name = ${IRCD_NAME}
+Info = ${IRCD_INFO}
+Listen = 0.0.0.0
+MotdPhrase = ${IRCD_MOTD}
+Network = ${IRCD_NETWORK}
+Ports = ${IRCD_PORTS}
+ServerGID = nobody
+ServerUID = nobody
 
-# Handle server link configuration
-IRCD_LINK_CONFIG=""
-if [ -n "$IRCD_LINK_NAME" ]; then
-    IRCD_LINK_CONFIG="${IRCD_LINK_CONFIG}Name = ${IRCD_LINK_NAME}
-"
-fi
-if [ -n "$IRCD_LINK_HOST" ]; then
-    IRCD_LINK_CONFIG="${IRCD_LINK_CONFIG}Host = ${IRCD_LINK_HOST}
-"
-fi
-if [ -n "$IRCD_LINK_PORT" ]; then
-    IRCD_LINK_CONFIG="${IRCD_LINK_CONFIG}Port = ${IRCD_LINK_PORT}
-"
-fi
-if [ -n "$IRCD_LINK_PASSWORD" ]; then
-    IRCD_LINK_CONFIG="${IRCD_LINK_CONFIG}MyPassword = ${IRCD_LINK_PASSWORD}
-"
-fi
-if [ -n "$IRCD_LINK_PEER_PASSWORD" ]; then
-    IRCD_LINK_CONFIG="${IRCD_LINK_CONFIG}PeerPassword = ${IRCD_LINK_PEER_PASSWORD}
-"
-fi
+[Limits]
+MaxListSize = 100
+PingTimeout = 120
 
-export IRCD_LINK_CONFIG
-export IRCD_SSL_CONFIG
+[Options]
+DNS = yes
+PAM = no
 
-echo "Writing ngircd.conf"
-envsubst < /ngircd.conf.tmpl > /ngircd.conf
+[SSL]
+${IRCD_SSL_CERT_FILE:+CertFile = ${IRCD_SSL_CERT_FILE}}
+${IRCD_SSL_KEY_FILE:+KeyFile = ${IRCD_SSL_KEY_FILE}}
+${IRCD_SSL_KEYFILE_PASSWORD:+KeyFilePassword = ${IRCD_SSL_KEYFILE_PASSWORD}}
+${IRCD_SSL_PORTS:+Ports = ${IRCD_SSL_PORTS}}
 
-# test config
+[Server]
+SSLVerify = no
+SSLConnect = yes
+${IRCD_LINK_NAME:+Name = ${IRCD_LINK_NAME}}
+${IRCD_LINK_HOST:+Host = ${IRCD_LINK_HOST}}
+${IRCD_LINK_PORT:+Port = ${IRCD_LINK_PORT}}
+${IRCD_LINK_PASSWORD:+MyPassword = ${IRCD_LINK_PASSWORD}}
+${IRCD_LINK_PEER_PASSWORD:+PeerPassword = ${IRCD_LINK_PEER_PASSWORD}}
+EOF
+
+# Test config
 /usr/sbin/ngircd -f /ngircd.conf -t
+
 # Start ngIRCd with debug logging
 exec /usr/sbin/ngircd -f /ngircd.conf -n -d
